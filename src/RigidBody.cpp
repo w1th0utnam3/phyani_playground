4007671283e689f6a8cecb102b8f791184b03fa4
Fabian Loeschner <fabian.loeschner@rwth-aachen.de>
2017-05-22 17:28:10 +0200

    Added methods to transform from and to local coordinates

--- src/RigidBody.cpp
+++ src/RigidBody.cpp
@@ -5,11 +5,18 @@ RigidBody::RigidBody()
 {
 }
 
+RigidBody::RigidBody(const Properties& properties)
+	: m_static(false)
+	, m_properties(properties)
+{
+}
+
 RigidBody::RigidBody(const Properties& properties, const State& initialState)
 	: m_static(false)
 	, m_properties(properties)
 	, m_state(initialState)
 {
+	updateMatrices();
 }
 
 RigidBody::Properties RigidBody::properties() const
@@ -22,15 +29,30 @@ RigidBody::State RigidBody::state() const
 	return m_state;
 }
 
+void RigidBody::setState(const RigidBody::State& state)
+{
+	m_state = state;
+	updateMatrices();
+}
+
+Eigen::Vector3d RigidBody::toLocalCoordinates(const Eigen::Vector3d& globalCoordinates) const
+{
+	return m_rotationMatrix*globalCoordinates + m_state.position;
+}
+
+Eigen::Vector3d RigidBody::toGlobalCoordinates(const Eigen::Vector3d& localCoordinates) const
+{
+	return m_inverseRotationMatrix*(localCoordinates - m_state.position);
+}
+
 Eigen::Matrix3d RigidBody::globalInertia() const
 {
-	return (m_state.attitude * m_properties.inertia.asDiagonal()) * m_state.attitude;
+	return (m_rotationMatrix * m_properties.inertia.asDiagonal()) * m_rotationMatrix.transpose();
 }
 
 Eigen::Matrix3d RigidBody::inverseGlobalInertia() const
 {
-	Eigen::Matrix3d rotation = m_state.attitude.toRotationMatrix();
-	return (rotation * m_properties.inertia.cwiseInverse().asDiagonal()) * rotation;
+	return (m_rotationMatrix * m_properties.inertia.cwiseInverse().asDiagonal()) * m_rotationMatrix.transpose();
 }
 
 RigidBody::State RigidBody::evaluateEquationsOfMotion(double t, const State& state, const RigidBody& body)
@@ -47,7 +69,7 @@ RigidBody::State RigidBody::evaluateEquationsOfMotion(double t, const State& sta
 		0.5*state.angularVelocity(1), 
 		0.5*state.angularVelocity(2)
 	);
-	stateChange.attitude = angularVelocityQuat*state.attitude;
+	stateChange.rotation = angularVelocityQuat*state.rotation;
 
 	// Linear acceleration
 	Eigen::Vector3d force = Eigen::Vector3d(0, 0, 0);
@@ -58,4 +80,11 @@ RigidBody::State RigidBody::evaluateEquationsOfMotion(double t, const State& sta
 	stateChange.angularVelocity = body.inverseGlobalInertia()*(torque - state.angularVelocity.cross(body.globalInertia()*state.angularVelocity));
 
 	return stateChange;
+}
+
+void RigidBody::updateMatrices()
+{
+	m_state.rotation.normalize();
+	m_rotationMatrix = m_state.rotation.toRotationMatrix();
+	m_inverseRotationMatrix = m_state.rotation.conjugate().toRotationMatrix();
 }
