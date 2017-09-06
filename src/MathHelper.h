4c6e0cb4bf26a8c7efd0dc5a792a5a5e8e26fc9d
Fabian Loeschner <fabian.loeschner@rwth-aachen.de>
2017-09-06 19:57:05 +0200

    Updated several submodules

--- src/MathHelper.h
+++ src/MathHelper.h
@@ -5,21 +5,21 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
 
-template <typename T, glm::precision P>
+template <typename T, glm::qualifier P>
 std::ostream& operator<<(std::ostream& os, glm::tquat<T, P> quat)
 {
 	os << "w=" << quat.w << ",x=" << quat.x << ",y=" << quat.y << ",z=" << quat.z;
 	return os;
 }
 
-template <typename T, glm::precision P>
+template <typename T, glm::qualifier P>
 std::ostream& operator<<(std::ostream& os, glm::tvec3<T, P> vec)
 {
 	os << "x=" << vec.x << ",y=" << vec.y << ",z=" << vec.z;
 	return os;
 }
 
-template <glm::length_t l, typename T, glm::precision P>
+template <glm::length_t l, typename T, glm::qualifier P>
 std::ostream& operator<<(std::ostream& os, glm::vec<l, T, P> vec)
 {
 	os << vec[0];
@@ -28,7 +28,7 @@ std::ostream& operator<<(std::ostream& os, glm::vec<l, T, P> vec)
 }
 
 
-template <typename T, glm::precision P>
+template <typename T, glm::qualifier P>
 bool isfinite(glm::tquat<T, P> quat)
 {
 	return (std::isfinite(quat.x)
@@ -37,7 +37,7 @@ bool isfinite(glm::tquat<T, P> quat)
 	        && std::isfinite(quat.w));
 }
 
-template <typename T, glm::precision P>
+template <typename T, glm::qualifier P>
 bool isfinite(glm::tvec3<T, P> vec)
 {
 	return (std::isfinite(vec.x)
