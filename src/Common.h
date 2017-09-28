e54f9074d06f660251a669706f9b6032816f2a44
Fabian Loeschner <fabian.loeschner@rwth-aachen.de>
2017-09-28 19:55:29 +0200

    Current libstdc++ variant does not work with the code

--- src/Common.h
+++ src/Common.h
@@ -2,7 +2,8 @@
 #if defined(__cpp_lib_variant) && __cpp_lib_variant >= 201606
 	#define HAS_STD_VARIANT
 #elif defined(__GNUC__) && __GNUC__ >= 7
-	#define HAS_STD_VARIANT
+	// Current variant implementation has bugs
+	//#define HAS_STD_VARIANT
 #elif defined(_MSC_VER) && _MSC_VER >= 1910
 	#define HAS_STD_VARIANT
 #elif defined(__clang_major__) && __clang_major__ >= 4
