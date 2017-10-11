6fb0f7ef6a4ebaf9ea342185e48c6efd933a95ca
Fabian Loeschner <fabian.loeschner@rwth-aachen.de>
2017-10-11 18:43:56 +0200

    Force Clang 5.0 on linux with libstdc++ to use the mpark variant

--- src/Common.h
+++ src/Common.h
@@ -4,16 +4,17 @@
 #include <ciso646>
 
 #if defined(__clang_major__) && __clang_major__ < 5 && defined(__GLIBCXX__)
-	#error Clang/LLVM older than version 5.0.0 is incompatible with libstdc++ in C++17 mode
+	#error Clang/LLVM older than version 5.0.0 is incompatible with libstdc++ <functional> in C++17 mode (missing template deduction guides)
 #endif
 
 // Conditionally switch on std::variant support
 #if __has_include(<variant>) || (defined(__cpp_lib_variant) && __cpp_lib_variant >= 201606)
 	#define HAS_STD_VARIANT
-#elif defined(__GNUC__) && __GNUC__ >= 7
-	#define HAS_STD_VARIANT
-#elif defined(_MSC_VER) && _MSC_VER >= 1910
-	#define HAS_STD_VARIANT
+#endif
+
+// Clang 5.0 is incompatible with current libstdc++ std::variant implementation
+#if defined(__clang_major__) && defined(__GLIBCXX__)
+//	#undef HAS_STD_VARIANT
 #endif
 
 // Switch variant implementation depending on std library support
