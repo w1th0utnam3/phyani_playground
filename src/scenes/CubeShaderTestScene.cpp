f14da5717b3a7769e5c56c21a748526c3112e52e
Fabian Loeschner <fabian.loeschner@rwth-aachen.de>
2017-10-15 23:36:37 +0200

    Updated the shader test scene to make use of the new drawable system

--- src/scenes/CubeShaderTestScene.cpp
+++ src/scenes/CubeShaderTestScene.cpp
@@ -4,83 +4,11 @@
 #include <utility>
 
 #include "CommonOpenGl.h"
-
-using Triangle = glm::fvec3[3];
-
-static const Triangle cube_triangles[] = {
-	{{-0.5f,-0.5f,-0.5f},
-	 {-0.5f,-0.5f, 0.5f},
-	 {-0.5f, 0.5f, 0.5f}},
-
-	{{ 0.5f, 0.5f,-0.5f},
-	 {-0.5f,-0.5f,-0.5f},
-	 {-0.5f, 0.5f,-0.5f}},
-
-	{{ 0.5f,-0.5f, 0.5f},
-	 {-0.5f,-0.5f,-0.5f},
-	 { 0.5f,-0.5f,-0.5f}},
-
-	{{ 0.5f, 0.5f,-0.5f},
-	 { 0.5f,-0.5f,-0.5f},
-	 {-0.5f,-0.5f,-0.5f}},
-
-	{{-0.5f,-0.5f,-0.5f},
-	 {-0.5f, 0.5f, 0.5f},
-	 {-0.5f, 0.5f,-0.5f}},
-
-	{{ 0.5f,-0.5f, 0.5f},
-	 {-0.5f,-0.5f, 0.5f},
-	 {-0.5f,-0.5f,-0.5f}},
-
-	{{-0.5f, 0.5f, 0.5f},
-	 {-0.5f,-0.5f, 0.5f},
-	 { 0.5f,-0.5f, 0.5f}},
-
-	{{ 0.5f, 0.5f, 0.5f},
-	 { 0.5f,-0.5f,-0.5f},
-	 { 0.5f, 0.5f,-0.5f}},
-
-	{{ 0.5f,-0.5f,-0.5f},
-	 { 0.5f, 0.5f, 0.5f},
-	 { 0.5f,-0.5f, 0.5f}},
-
-	{{ 0.5f, 0.5f, 0.5f},
-	 { 0.5f, 0.5f,-0.5f},
-	 {-0.5f, 0.5f,-0.5f}},
-
-	{{ 0.5f, 0.5f, 0.5f},
-	 {-0.5f, 0.5f,-0.5f},
-	 {-0.5f, 0.5f, 0.5f}},
-
-	{{ 0.5f, 0.5f, 0.5f},
-	 {-0.5f, 0.5f, 0.5f},
-	 { 0.5f,-0.5f, 0.5f}}
-};
+#include "DrawableFactory.h"
 
 void CubeShaderTestScene::initializeSceneContent()
 {
-	const std::size_t component_count = (sizeof(cube_triangles) / sizeof(Triangle)) * 3 * 3;
-	m_vertices.reserve(component_count);
-	m_normals.reserve(component_count);
-
-	for (const auto& triangleVertices : cube_triangles) {
-		// Store all vertex components of the triangle
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				m_vertices.push_back(triangleVertices[i][j]);
-			}
-		}
-
-		// Compute the triangle normal
-		glm::fvec3 edge1 = triangleVertices[1] - triangleVertices[2];
-		glm::fvec3 edge2 = triangleVertices[2] - triangleVertices[0];
-		glm::fvec3 normal = glm::normalize(glm::cross(edge1, edge2));
-
-		// Store triangle normal for each vertex
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) m_normals.push_back(normal[j]);
-		}
-	}
+	m_cubeDrawableId = m_drawables.registerDrawable(DrawableFactory::createCube());
 
 	// The number of cubes per edge of the "cube grid"
 	const int edgeLength = 2;
@@ -95,17 +23,44 @@ void CubeShaderTestScene::initializeSceneContent()
 	//const int edgeLength = 42;
 	//const int edgeLength = 72;
 
-	// Generate the initial cube grid
 	const glm::fmat4 id(1.0f);
-	m_model_mats.reserve(instanceCount);
-	m_model_colors.reserve(instanceCount);
-	for (int i = 0; i < edgeLength; i++) {
-		for (int j = 0; j < edgeLength; j++) {
-			for (int k = 0; k < edgeLength; k++) {
-				m_model_mats.push_back(glm::translate(id, glm::fvec3(i*distance - disp, j*distance - disp, k*distance - disp)));
-				m_model_colors.emplace_back(1.0f, 0.0f, 0.0f);
+
+	// Generate the initial cube grid
+	{
+		GLuint instanceOffset = m_drawables.createInstances(m_cubeDrawableId, instanceCount);
+
+		auto bufferLock = m_drawables.shared_lock();
+		auto drawableData = m_drawables.drawable(m_cubeDrawableId);
+		InstanceData* data = drawableData.instanceData() + instanceOffset;
+
+		for (int i = 0; i < edgeLength; i++) {
+			for (int j = 0; j < edgeLength; j++) {
+				for (int k = 0; k < edgeLength; k++) {
+					glm::fmat4* model_mat = reinterpret_cast<glm::fmat4*>(&data->model_mat);
+					*model_mat = glm::translate(id, glm::fvec3(i*distance - disp, j*distance - disp, k*distance - disp));
+
+					data->color[0] = 255;
+					data->color[1] = 0;
+					data->color[2] = 0;
+					data->color[3] = 0;
+
+					data++;
+				}
 			}
 		}
+	}
+
+	{
+		InstanceData lightCube;
+		lightCube.color[0] = 0;
+		lightCube.color[1] = 0;
+		lightCube.color[2] = 255;
+		lightCube.color[3] = 0;
+		glm::fmat4* model_mat = reinterpret_cast<glm::fmat4*>(lightCube.model_mat);
+		*model_mat = glm::translate(id, glm::fvec3(0.5f, 1.0f, 6.0f));
+		*model_mat = glm::scale(*model_mat, glm::fvec3(0.2f, 0.2f, 0.2f));
+
+		m_drawables.storeInstance(m_cubeDrawableId, lightCube);
 	}
 
 	glGenVertexArrays(1, &m_vao);
@@ -114,22 +69,17 @@ void CubeShaderTestScene::initializeSceneContent()
 	// Generate buffer for vertex positions
 	glGenBuffers(1, &m_vertex_buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, m_vertex_buffer);
-	glBufferData(GL_ARRAY_BUFFER, m_vertices.size() * sizeof(GLfloat), m_vertices.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, m_drawables.vertexBufferSize(), m_drawables.vertexBufferData(), GL_STATIC_DRAW);
 
 	// Generate buffer for vertex normals
 	glGenBuffers(1, &m_normal_buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, m_normal_buffer);
-	glBufferData(GL_ARRAY_BUFFER, m_normals.size() * sizeof(GLfloat), m_normals.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, m_drawables.normalBufferSize(), m_drawables.normalBufferData(), GL_STATIC_DRAW);
 
-	// Generate buffer for model colors
-	glGenBuffers(1, &m_model_color_buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, m_model_color_buffer);
-	glBufferData(GL_ARRAY_BUFFER, m_model_colors.size() * 3 * sizeof(GLfloat), glm::value_ptr(m_model_colors[0]), GL_STATIC_DRAW);
-
-	// Generate buffer for model matrices
-	glGenBuffers(1, &m_model_mat_buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, m_model_mat_buffer);
-	glBufferData(GL_ARRAY_BUFFER, m_model_mats.size() * (4*4) * sizeof(GLfloat), glm::value_ptr(m_model_mats[0]), GL_STREAM_DRAW);
+	// Generate buffer for model matrices and colors
+	glGenBuffers(1, &m_instance_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, m_instance_buffer);
+	glBufferData(GL_ARRAY_BUFFER, instanceCount * sizeof(InstanceData), NULL, GL_STREAM_DRAW);
 
 	// Compile the shader and get attribute locations
 	{
@@ -150,28 +100,35 @@ void CubeShaderTestScene::initializeSceneContent()
 		m_vert_norm_location = glGetAttribLocation(m_shaderProgram.program(), "vertexNormal_modelspace");
 	}
 
+	const std::size_t fvec3_size = sizeof(GLfloat) * 3;
+	const std::size_t fvec4_size = sizeof(GLfloat) * 4;
+
+	InstanceData instanceData;
+	const std::size_t color_offset = 0;
+	const std::size_t model_mat_offset = reinterpret_cast<char*>(&instanceData.model_mat) - reinterpret_cast<char*>(&instanceData.color[0]);
+
 	// Set the vertex attribute pointers for the vertex positions
 	glBindBuffer(GL_ARRAY_BUFFER, m_vertex_buffer);
 	glEnableVertexAttribArray(m_vert_pos_location);
-	glVertexAttribPointer(m_vert_pos_location, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 3, (void*) 0);
+	glVertexAttribPointer(m_vert_pos_location, 3, GL_FLOAT, GL_FALSE, fvec3_size, (void*) 0);
 	glVertexAttribDivisor(m_vert_pos_location, 0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, m_normal_buffer);
 	glEnableVertexAttribArray(m_vert_norm_location);
-	glVertexAttribPointer(m_vert_norm_location, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 3, (void*) 0);
+	glVertexAttribPointer(m_vert_norm_location, 3, GL_FLOAT, GL_FALSE, fvec3_size, (void*) 0);
 	glVertexAttribDivisor(m_vert_norm_location, 0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, m_model_color_buffer);
+	// Set the vertex attribute pointers for the colors model
+	glBindBuffer(GL_ARRAY_BUFFER, m_instance_buffer);
 	glEnableVertexAttribArray(m_model_color_location);
-	glVertexAttribPointer(m_model_color_location, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 3, (void*) 0);
+	glVertexAttribPointer(m_model_color_location, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(InstanceData), (void*)color_offset);
 	glVertexAttribDivisor(m_model_color_location, 1);
 
 	// Set the vertex attribute pointers for the model matrices (matrix is represented by 4 vectors)
-	glBindBuffer(GL_ARRAY_BUFFER, m_model_mat_buffer);
 	for (int i = 0; i < 4; i++) {
 		glEnableVertexAttribArray(m_model_mat_location + i);
 		glVertexAttribPointer(m_model_mat_location + i, 4, GL_FLOAT, GL_FALSE,
-							  sizeof(GLfloat) * 4 * 4, (void*)(sizeof(GLfloat) * i * 4));
+							  sizeof(InstanceData), (void*)(model_mat_offset + fvec4_size * i));
 		// Set the divisor so that one model matrix is used for every instance instead of every vertex
 		glVertexAttribDivisor(m_model_mat_location + i, 1);
 	}
@@ -185,42 +142,55 @@ void CubeShaderTestScene::cleanupSceneContent()
 {
 	glDeleteVertexArrays(1, &m_vao);
 	glDeleteBuffers(1, &m_vertex_buffer);
-	glDeleteBuffers(1, &m_model_mat_buffer);
+	glDeleteBuffers(1, &m_normal_buffer);
+	glDeleteBuffers(1, &m_instance_buffer);
+
+	m_drawables.clear();
 }
 
 void CubeShaderTestScene::renderSceneContent()
 {
+	auto bufferLock = m_drawables.shared_lock();
+
 	const glm::fmat4 v = m_camera->viewMatrix();
 	const glm::fmat4 p = m_camera->projectionMatrix();
 
 	// Get dt since last render for constant rotation speed
 	const double currentTime = glfwGetTime();
 	const double dt = currentTime - m_lastTime;
-	m_lastTime = currentTime;
+	m_lastTime = currentTime;	
 
-	// Rotate all model matrices in random directions
-	/*
-	for (auto& modelMat : m_model_mats)
-		modelMat = glm::rotate(modelMat, (float)(2*dt), glm::fvec3(m_random(), m_random(), m_random()));
-	*/
+	for (auto it = m_drawables.drawablesBegin(); it != m_drawables.drawablesEnd(); ++it) {
+		auto drawableData = *it;
 
-	if (CommonOpenGl::getGlValue<GLint>(GL_VERTEX_ARRAY_BINDING) != m_vao)
-		glBindVertexArray(m_vao);
+		// Rotate all model matrices in random directions
+		/*
+		for (auto& modelMat : m_model_mats)
+			modelMat = glm::rotate(modelMat, (float)(2*dt), glm::fvec3(m_random(), m_random(), m_random()));
+		*/
 
-	// Update the model matrix buffer
-	const int cubeCount = m_model_mats.size();
-	glBindBuffer(GL_ARRAY_BUFFER, m_model_mat_buffer);
-	glBufferData(GL_ARRAY_BUFFER, cubeCount * (4*4) * sizeof(GLfloat), NULL, GL_STREAM_DRAW);
-	glBufferSubData(GL_ARRAY_BUFFER, 0, cubeCount * (4*4) * sizeof(GLfloat), glm::value_ptr(m_model_mats[0]));
+		if (CommonOpenGl::getGlValue<GLint>(GL_VERTEX_ARRAY_BINDING) != m_vao)
+			glBindVertexArray(m_vao);
 
-	m_shaderProgram.useProgram();
+		// Update the model matrix buffer
+		glBindBuffer(GL_ARRAY_BUFFER, m_instance_buffer);
+		glBufferData(GL_ARRAY_BUFFER, drawableData.instanceDataSize(), NULL, GL_STREAM_DRAW);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, drawableData.instanceDataSize(), drawableData.instanceData());
 
-	// Update the view and projection matrices
-	glUniformMatrix4fv(m_view_mat_location, 1, GL_FALSE, (const GLfloat*) glm::value_ptr(v));
-	glUniformMatrix4fv(m_projection_mat_location, 1, GL_FALSE, (const GLfloat*) glm::value_ptr(p));
-	// Draw multiple instances of the cube
-	glDrawArraysInstanced(GL_TRIANGLES, 0, m_vertices.size() / 3, cubeCount);
+		m_shaderProgram.useProgram();
 
-	//glUseProgram(0);
-	//glBindVertexArray(0);
+		// Update the view and projection matrices
+		glUniformMatrix4fv(m_view_mat_location, 1, GL_FALSE, (const GLfloat*) glm::value_ptr(v));
+		glUniformMatrix4fv(m_projection_mat_location, 1, GL_FALSE, (const GLfloat*) glm::value_ptr(p));
+
+		// Draw multiple instances of the cube
+		const GLuint instanceCount = drawableData.instanceCount();
+		const GLuint drawableVertexCount = drawableData.vertexCount;
+
+		if (instanceCount > 1) {
+			glDrawArraysInstanced(drawableData.mode, 0, drawableVertexCount / 3, instanceCount);
+		} else {
+			glDrawArrays(drawableData.mode, 0, drawableVertexCount / 3);
+		}
+	}
 }
